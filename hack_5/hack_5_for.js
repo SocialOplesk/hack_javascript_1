3e1c82d51a4af6fb425374b4f8b87ab29be297f1
Yelitza Suniaga <ysuniaga1981@gmail.com>
2024-03-07 21:50:00 -0400

    second commit

--- hack_5/hack_5_for.js
+++ hack_5/hack_5_for.js
@@ -7,6 +7,10 @@
  */
 let result = [];
 
+let i = 7;
+for(;i>=1;i--){
+    if(i % 2 !== 0) result.push(i)
+}
 
 //export result
 module.exports = result;
--- hack_5/hack_5_while.js
+++ hack_5/hack_5_while.js
@@ -7,6 +7,14 @@
  */
 let result = [];
 
+let i = 7;
+
+while (i>=1){
+
+    if(i % 2 !== 0) result.push(i);
+    i--;
+};
+
 
 //export result
 module.exports = result;
